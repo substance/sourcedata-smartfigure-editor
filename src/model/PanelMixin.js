537138a6b6be40bffcd57e0ab1eebed217e68dd0
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-12-05 22:46:02 +0100

    Fix PanelMixin.

--- src/model/PanelMixin.js
+++ src/model/PanelMixin.js
@@ -1,22 +1,18 @@
 export default function PanelMixin (_PanelNode) {
   return class Panel extends _PanelNode {
     getTemplate () {
-      const template = Panel.getTemplate()
-      template.metadata = this.resolve('keywords').map(keyword => (
-        { type: 'structured-keyword', name: keyword.name, value: '' }
-      ))
-      return template
-    }
-
-    static getTemplate () {
       return {
         type: 'panel',
         image: { type: 'image' },
         legend: [{ type: 'paragraph' }],
-        metadata: [
-          { type: 'metadata-field', name: 'Exp. System' },
-          { type: 'metadata-field', name: 'Measured Variables' }
-        ]
+        // NOTE: using the same keyword groups as in the present panel
+        keywords: this.resolve('keywords').map(keywordGroup => {
+          return {
+            type: 'keyword-group',
+            name: keywordGroup.name,
+            keywords: [{ type: 'keyword', content: '' }]
+          }
+        })
       }
     }
   }
