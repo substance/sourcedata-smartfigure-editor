9b539ca3e3fffc63de43f2efdb20b0ab9caff600
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-12-06 19:18:53 +0100

    Tidy up.

--- src/components/AttachedResourcesComponent.js
+++ src/components/AttachedResourcesComponent.js
@@ -1,4 +1,5 @@
-import { $$, PropertyComponent, SelectableNodeComponent, domHelpers } from 'substance'
+import { $$, PropertyComponent, domHelpers } from 'substance'
+import AttachedResourceComponent from './AttachedResourceComponent'
 
 export default class AttachedResourcesComponent extends PropertyComponent {
   getPath () {
@@ -26,31 +27,5 @@ export default class AttachedResourcesComponent extends PropertyComponent {
   _onMousedown (panel, resourceNode, event) {
     domHelpers.stopAndPrevent(event)
     this.context.api.selectValue(panel, 'resources', resourceNode.id)
-  }
-}
-
-class AttachedResourceComponent extends SelectableNodeComponent {
-  render () {
-    const { node } = this.props
-    const { selected } = this.state
-    const el = $$('button', { class: 'sc-attached-resource', 'data-id': this._getSelectableId() })
-    if (selected) el.addClass('sm-selected')
-    if (node.title) {
-      el.append(
-        $$('span', { class: 'se-title' }, node.title)
-      )
-      if (node.href) el.append(': ')
-    }
-    if (node.href) {
-      el.append(
-        $$('span', { class: 'se-href' }, node.href)
-      )
-    }
-    return el
-  }
-
-  _getSelectableId () {
-    const { panel, node } = this.props
-    return `${panel.id}.resources#${node.id}`
   }
 }
